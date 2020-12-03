f48ef6d2261714bae3201545a4b81c6360e28798
Ankit Singh <ankitas90@gmail.com>
2020-12-04 01:06:17 +0530

    Signin and Signout feature

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -5,19 +5,9 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   def create
     user = User.new(users_params)
     if user.save
-      render json: {
-        messages: "Signed Up Successfully",
-        is_success: true,
-        data: {
-          user: user
-        }
-      }, status: :ok
+      json_response "Signed Up Successfully", true, {user: user}, :ok
     else
-      render json: {
-        messages: "Something wrong",
-        is_success: false,
-        data: {}
-      }, status: :unprocessable_entity
+      json_response "Something wrong", false, {}, :unprocessable_entity
     end
   end
 
@@ -29,10 +19,6 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
   def ensure_params_exist
     return if params[:user].present?
-    render json: {
-      messages: "Missing params",
-      is_success: false,
-      data: {}
-    }, status: :bad_request
+    json_response "Missing params", false, {}, :bad_request
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       devise_scope :user do
         post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+        delete "logout", to: "sessions#destroy"
       end
     end
   end
